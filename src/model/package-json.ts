b92c4a6fdaf13bd9bc6143e633513695bd380209
Kamen Minkov <kamen.minkov@gmail.com>
2021-05-03 01:53:54 +0300

    Show existing themes in a quick pick list

---
+++ src/model/package-json.ts
@@ -0,0 +1,68 @@
+export interface PackageJson {
+	name: string;
+	displayName: string;
+	description: string;
+	version: string;
+	publisher: string;
+	icon: string;
+	repository: Repository;
+	homepage: string;
+	bugs: Bugs;
+	engines: Engines;
+
+	categories: string[];
+	keywords: string[];
+	badges: Badge[];
+	activationEvents: string[];
+	main: string;
+	contributes: Contributes;
+	license: string;
+	author: string;
+	devDependencies: { [key: string]: string };
+	scripts: Scripts;
+	private: boolean;
+}
+
+export interface Badge {
+	url: string;
+	href: string;
+	description: string;
+}
+
+export interface Bugs {
+	url: string;
+}
+
+interface Contributes {
+	commands: Command[];
+	themes: Theme[];
+}
+
+export interface Theme {
+	label: string;
+	uiTheme: UITheme;
+	path: string;
+}
+
+export enum UITheme {
+	Vs = "vs",
+	VsDark = "vs-dark"
+}
+
+interface Engines {
+	vscode: string;
+}
+
+export interface Repository {
+	url: string;
+	type: string;
+}
+
+interface Command {
+	command: string;
+	title: string;
+}
+
+interface Scripts {
+	[key: string]: string;
+}
